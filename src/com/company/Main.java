0660953396b2b9b97b2394c7f4beec7e569bf511
Rainbowdm <dim8035@gmail.com>
2022-01-25 19:31:19 +0200

    Add Apple.applePrice

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,6 +16,7 @@ public class Main {
 
         public static void addPrice(int applesPrice) {
             Apple.applePrice = Apple.applePrice + applesPrice;
+            Apple.applePrice = Apple.applePrice + applesPrice;
             System.out.println("Стоимость " + applesPrice);
         }
     }
