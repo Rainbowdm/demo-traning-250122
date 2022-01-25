edcd4d5bfd88ae5a4d69967d0995a3a6373afeb9
Rainbowdm <dim8035@gmail.com>
2022-01-25 19:09:44 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+// Цена яблок
+public class Main {
+
+    public static void main(String[] args) {
+        Apple appleRed = new Apple();
+        appleRed.addPrice(50);
+        Apple apple = new Apple();
+        apple.addPrice(100);
+        System.out.println("Стоимость яблок " + Apple.applePrice);
+    }
+
+    public static class Apple {
+        public static int applePrice = 0;
+
+        public static void addPrice(int applesPrice) {
+            Apple.applePrice = Apple.applePrice + applesPrice;
+            System.out.println("Стоимость " + applesPrice);
+        }
+    }
+
+}
